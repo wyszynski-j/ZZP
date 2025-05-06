8523304997bb92c8a00988a3eca550dedb28c874
Jakub Wyszyński <135658962+wyszynski-j@users.noreply.github.com>
2025-05-06 09:51:41 +0200

    initial commit

---
+++ zzp/zzp/src/main/java/com/example/zzp/ZzpApplication.java
@@ -0,0 +1,13 @@
+package com.example.zzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzpApplication.class, args);
+	}
+
+}
---
+++ zzp/zzp/src/test/java/com/example/zzp/ZzpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zzp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
